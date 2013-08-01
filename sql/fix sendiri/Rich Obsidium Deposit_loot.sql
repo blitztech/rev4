25906ffdc2ba9329220746040e80dc5d9d4d6b27
blitztech <blitztech4@yahoo.com>
2013-08-02 00:16:05 +0800

    73680 Unleash Elements with sql/fix sendiri/73680 Unleash Elements.sql

---
+++ sql/fix sendiri/Rich Obsidium Deposit_loot.sql
@@ -0,0 +1,16 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.6.12 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202739','52177','1','1','0','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202739','52178','1','1','0','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202739','52179','1','1','0','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202739','52180','1','1','0','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202739','52181','1','1','0','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202739','52182','1','1','0','1','1');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202739','52327','4','1','0','3','4');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202739','52328','20','1','0','1','2');
+insert into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('202739','53038','100','1','0','5','12');
